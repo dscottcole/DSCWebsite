8b1fc2bf282c68f2719e5c3a880fe7071a30ab70
dscottcole <67556122+dscottcole@users.noreply.github.com>
2020-12-10 21:54:16 -0600

    About me section done, vertical mobile supported

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,8 +14,8 @@ function Home() {
                 </div>
                 <div className="home-text">
                     <Typography variant="h2"> Daniel Scott Cole </Typography>
-                    <Typography style={{ color: "rgba(191,191,191,1)" }} variant="h4">I'm a software engineer with experience in Javascript and Ruby frameworks.</Typography>
-                    <Typography style={{ color: "rgba(191,191,191,1)" }} variant="h4">I hope you enjoy checking out my website and portfolio.</Typography>
+                    <Typography style={{ color: "rgba(238,238,238,1)" }} variant="h4">I'm a software engineer with experience in Javascript and Ruby frameworks.</Typography>
+                    <Typography style={{ color: "rgba(238,238,238,1)" }} variant="h4">I hope you enjoy checking out my website and portfolio.</Typography>
                     <div className="home-bottom-text">
                         <Typography style={{ color: "rgba(255,100,0,1)" }} variant="h4">I built this website using React.js.</Typography>
                     </div>
